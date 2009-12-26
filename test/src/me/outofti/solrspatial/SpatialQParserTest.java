351f872fb1b5aca87d3fb0bb669311eb81ff3e8c
Mat Brown <mat@patch.com>
2009-12-25 19:46:38 -0500

    Beef up test for distance filter

--- test/src/me/outofti/solrspatial/SpatialQParserTest.java
+++ test/src/me/outofti/solrspatial/SpatialQParserTest.java
@@ -16,9 +16,10 @@ public class SpatialQParserTest extends TestHelper {
 
     @Test public void simpleSpatialSearch() throws Exception {
         final SolrServer server = getServer();
-        addLocation("New York", 40.7142691, -74.0059729); //  0.000 miles
-        addLocation("Brooklyn", 40.6501037, -73.9495823); //  5.328 miles
-        addLocation("Yonkers", 40.9312099, -73.8987469);  // 16.000 miles
+        addLocation("New York", 40.7142691, -74.0059729); //      0.000 miles
+        addLocation("Brooklyn", 40.6501037, -73.9495823); //      5.328 miles
+        addLocation("Staten Island", 40.5834379, -74.1495875); // 11.765 miles, but inside the bounding box
+        addLocation("Yonkers", 40.9312099, -73.8987469);  //      16.000 miles
         server.commit();
         final SolrQuery query = new SolrQuery();
         query.setQuery("*:*");
